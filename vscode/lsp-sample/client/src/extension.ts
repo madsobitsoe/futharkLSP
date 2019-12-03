a98d032c8dbc9fd0d8402a11943591aa7225e3c0
Balder <balderi@tsponline.dk>
2019-12-03 15:18:22 +0100

    client can talk to futhark lsp

--- vscode/lsp-sample/client/src/extension.ts
+++ vscode/lsp-sample/client/src/extension.ts
@@ -3,63 +3,61 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
+import * as vscode from 'vscode';
+import * as languageClient from 'vscode-languageclient';
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
 
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient';
+const languageServerPath = path.join(require('os').homedir(), "/.local/bin/");
 
-let client: LanguageClient;
-
-export function activate(context: ExtensionContext) {
+function activateLanguageServer(context: vscode.ExtensionContext) {
 	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
+	let serverModule: string = languageServerPath;
 	// The debug options for the server
 	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	//let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
+	if(!serverModule) throw new URIError("Cannot find the language server module.");
+	let workPath = path.dirname(serverModule);
+	console.log(`Use ${serverModule} as server module.`);
+	console.log(`Work path: ${workPath}.`);
+	
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
+	let serverOptions: languageClient.ServerOptions = {
+		run: { command: "futhark", args: ["lsp"], options: { cwd: workPath} },
+		debug: { command: "futhark", args: ["lsp"], options: { cwd: workPath} }
 	};
 
 	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
+	let clientOptions: languageClient.LanguageClientOptions = {
 		// Register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			fileEvents:vscode.workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	};
 
 	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
+	let client = new languageClient.LanguageClient(
+		'futharkServerTest',
 		'Language Server Example',
 		serverOptions,
 		clientOptions
 	);
 
 	// Start the client. This will also launch the server
-	client.start();
+	let disposable = client.start();
+	context.subscriptions.push(disposable);
 }
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+export function activate(context: vscode.ExtensionContext) {
+	console.log("futhark test server is activated!");
+	activateLanguageServer(context);
+	let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
+		vscode.window.showInformationMessage("Hello, World!");
+	});
+	context.subscriptions.push(disposable);
+}
+export function deactivate() {
 }
